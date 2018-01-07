f7a5a342aa33c02090177f19730b9ab5e44026f8
eliasnijim <33372570+eliasnijim@users.noreply.github.com>
2018-01-07 23:05:17 +0200

    fixing

--- FlowersSystemG7_Server/src/PacketSender/Command.java
+++ FlowersSystemG7_Server/src/PacketSender/Command.java
@@ -29,7 +29,8 @@ public enum Command implements Serializable
 	updateAccountsBycId	,
 	updateAccounts,
 	updateCustomer,
+	addFlower,
 	updateUser,
 	getColors,
-	addFlower
-	}
+	getDiscountsByBranch
+}
