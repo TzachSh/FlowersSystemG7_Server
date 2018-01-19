2d137a0763ad8e3b7e919b3088c0b7bb782952a7
OrCo27 <34770124+OrCo27@users.noreply.github.com>
2018-01-19 22:33:58 +0200

    Package Report upgraded to be more generic. Abstract class contains now all the operations, and each concrete report must implements the interface

--- FlowersSystemG7_Server/src/Reports/IReport.java
+++ FlowersSystemG7_Server/src/Reports/IReport.java
@@ -2,12 +2,22 @@ package Reports;
 
 
 /**
- * Interface that implements the whole data for each line in the report,
- * each field separators by the String array 
+ * Interface that implements the whole data for each line in the report,<br>
+ * Each field separators by the String array 
  */
  public interface IReport {
  	/**
- 	 * return the query for generation report
+ 	 * return the query for generation report<br>
+ 	 * The query must contains in the where condition the prepared statements in this order:<br>
+ 	 * <b>branchId, year, quarter</b><br><br>
+ 	 * In Example:
+ 	 * <pre>
+ 	 * {@code
+	 * SELECT * FROM reportTable
+	 * WHERE brId=? AND YEAR(creationDate)=? AND QUARTER(creationDate)=?
+	 * }
+	 * </pre>
+ 	 *  
  	 * @param branchId The branch id to perform the query
  	 */
  	String getQueryReport(int branchId);
